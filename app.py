24948eaae24de9fdde6b2710487abd80ec71dc00
Juan Patricio <juanpbottega@gmail.com>
2019-08-24 01:00:52 -0300

    postgres sql modifications

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_jwt import JWT
 from flask_restful import Api
@@ -8,7 +9,7 @@ from resources.store import Store, StoreList
 
 # configuracion
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db') #el segundo es el valor por defecto
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'pato'
 api = Api(app)
